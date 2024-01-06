5ff62c702789e2d9775cf773228fc94c82e3d83f
jaychang99 <jaychang99@gmail.com>
2024-01-07 02:26:53 +0900

    fix(Cookie): 젭라

--- src/service/login.service.js
+++ src/service/login.service.js
@@ -20,7 +20,7 @@ function login(req, res, next) {
     // set cookie key as user-uuid, and value as uuid, domain is api.yoriquiz.site expires in 1 day
 
     res.cookie("user-uuid", uuid, {
-      domain: "yoriquiz.site",
+      domain: ".yoriquiz.site",
       expires: new Date(Date.now() + 86400000),
       secure: true,    // this will set the Secure flag
       httpOnly: true   // this will set the HttpOnly flag
