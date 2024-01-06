4c2d37ba169a5e3b0bb0bfbdd4e372bddb4a9079
Gwon Hong <gwonhong.dev@gmail.com>
2024-01-06 22:48:43 +0900

    add: getChats, addChat service and route, applied to index.js

--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,10 @@
 const leaderboard = [];
+const userMap = new Map();
+const chats = {
+  MAX_LENGTH: 1000,
+  THRESHOLD: 2000, // THRESHOLD 보다 커지면 MAX_LENGTH가 되도록 자름
+  startIndex: 0,
+  data: [],
+};
 
-export {leaderboard};
+export { leaderboard, userMap, chats };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
 
 import express from "express";
 import leaderboardRouter from "./routes/leaderboard.route.js";
+import chatRouter from "./routes/chat.route.js";
 
 const app = express();
 const port = 8080;
 
 app.use("/leaderboard", leaderboardRouter);
+app.use("/chats", chatRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
