4831a07827e97bd74800a568c80e00b06d4df066
Gwon Hong <gwonhong.dev@gmail.com>
2024-01-07 03:34:53 +0900

    fix: add user router at index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@
 
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import leaderboardRouter from "./routes/leaderboard.route.js";
 import loginRouter from "./routes/login.route.js";
 import chatRouter from "./routes/chat.route.js";
-import cors from "cors";
 import gamesRouter from "./routes/games.route.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 const port = 8080;
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://local.yoriquiz.site", "https://yoriquiz.site"],
-  credentials: true
-}
+  origin: [
+    "http://localhost:3000",
+    "https://local.yoriquiz.site",
+    "https://yoriquiz.site",
+  ],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -26,6 +31,8 @@ app.use("/leaderboard", leaderboardRouter);
 app.use("/chats", chatRouter);
 app.use("/login", loginRouter);
 app.use("/games", gamesRouter);
+app.use("/users", userRouter);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
