3239b2c176b05a90df549ab0b7c5184ce7e30c97
jaychang99 <jaychang99@gmail.com>
2024-01-06 23:43:01 +0900

    feat(CORS): cors 전부허용

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import express from "express";
 import leaderboardRouter from "./routes/leaderboard.route.js";
 import loginRouter from "./routes/login.route.js";
 import chatRouter from "./routes/chat.route.js";
+import cors from "cors";
 
 const app = express();
 const port = 8080;
 
 // body parser
+const corsOptions = {
+  origin: '*',
+  credentials: true
+}
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/leaderboard", leaderboardRouter);
