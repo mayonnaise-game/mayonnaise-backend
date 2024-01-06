28628417482360c932a5df755931f754340d98b7
Gwon Hong <gwonhong.dev@gmail.com>
2024-01-06 23:25:59 +0900

    add: install cookie-parser, applied to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // bootstrap express app
 
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import leaderboardRouter from "./routes/leaderboard.route.js";
 import loginRouter from "./routes/login.route.js";
 import chatRouter from "./routes/chat.route.js";
@@ -9,14 +11,17 @@ const app = express();
 const port = 8080;
 
 // body parser
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// cookie parser
+app.use(cookieParser());
+
+// routes
 app.use("/leaderboard", leaderboardRouter);
 app.use("/chats", chatRouter);
+app.use("/login", loginRouter);
 
-app.use("/leaderboard", leaderboardRouter);
-app.use("/login", loginRouter)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
