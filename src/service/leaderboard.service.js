4fe1fc996accf023f78795c0ec72fe98d2c15c61
Gwon Hong <gwonhong.dev@gmail.com>
2024-01-07 00:36:17 +0900

    fix: get username, score from userMap instead of request body

--- src/service/leaderboard.service.js
+++ src/service/leaderboard.service.js
@@ -1,4 +1,4 @@
-import { leaderboard, writeLeaderboard } from "../globals.js";
+import { userMap, leaderboard, writeLeaderboard } from "../globals.js";
 
 const LEADERBOARD_MAX_LENGTH = 100;
 
@@ -7,20 +7,33 @@ const getLeaderboard = (req, res, next) => {
 };
 
 const addLeaderboard = (req, res, next) => {
-  const { username, score } = req.body;
-  const newEntry = { username, score };
-  for (let i = 0; i < leaderboard.length; i++) {
-    const currentEntry = leaderboard[i];
-    if (newEntry.score > currentEntry.score) {
-      leaderboard.splice(i, 0, newEntry);
-      break;
+  const { "user-uuid": userUuid } = req.cookies;
+  try {
+    authUuid(userUuid);
+
+    const { username, score } = userMap.get(userUuid);
+    const newEntry = { username, score };
+    for (let i = 0; i < leaderboard.length; i++) {
+      const currentEntry = leaderboard[i];
+      if (newEntry.score > currentEntry.score) {
+        leaderboard.splice(i, 0, newEntry);
+        break;
+      }
     }
+    if (leaderboard.length > LEADERBOARD_MAX_LENGTH) {
+      leaderboard = leaderboard.slice(0, LEADERBOARD_MAX_LENGTH);
+    }
+    res.send("success");
+    writeLeaderboard();
+  } catch (err) {
+    console.error(err);
+
+    res.status(401);
+    res.json({
+      error: true,
+      message: "Invalid Request",
+    });
   }
-  if (leaderboard.length > LEADERBOARD_MAX_LENGTH) {
-    leaderboard = leaderboard.slice(0, LEADERBOARD_MAX_LENGTH);
-  }
-  res.send("success");
-  writeLeaderboard();
 };
 
 export { getLeaderboard, addLeaderboard };
