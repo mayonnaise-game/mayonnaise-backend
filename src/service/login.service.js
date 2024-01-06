8a4c9a67e0ef0199fdaff0231ec5a837fda4b156
Gwon Hong <gwonhong.dev@gmail.com>
2024-01-07 05:53:45 +0900

    fix: login.service.js remove set cookie

--- src/service/login.service.js
+++ src/service/login.service.js
@@ -17,17 +17,17 @@ function login(req, res, next) {
   };
 
   try {
-    res.setHeader("Content-Type", "application/json");
-    // set cookie key as user-uuid, and value as uuid, domain is api.yoriquiz.site expires in 1 day
+    // res.setHeader("Content-Type", "application/json");
+    // // set cookie key as user-uuid, and value as uuid, domain is api.yoriquiz.site expires in 1 day
 
-    res.cookie("user-uuid", uuid, {
-      maxAge: 365 * 24 * 60 * 60 * 1000, // Cookie expiration time in milliseconds (1 year in this example)
-      httpOnly: true, // this will set the HttpOnly flag
-      secure: true, // this will set the Secure flag
-      sameSite: "None",
-      domain: ".yoriquiz.site",
-      // expires: new Date(Date.now() + 86400000),
-    });
+    // res.cookie("user-uuid", uuid, {
+    //   maxAge: 365 * 24 * 60 * 60 * 1000, // Cookie expiration time in milliseconds (1 year in this example)
+    //   httpOnly: true, // this will set the HttpOnly flag
+    //   secure: true, // this will set the Secure flag
+    //   sameSite: "None",
+    //   domain: ".yoriquiz.site",
+    //   // expires: new Date(Date.now() + 86400000),
+    // });
     res.json(result);
   } catch (err) {
     console.error("Error while getting programming languages: ", err.message);
